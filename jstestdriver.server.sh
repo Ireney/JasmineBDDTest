17b11d16d3e365d3e9f44322c26bddc652a132d3
Ireney <ireney@navnet.com>
2010-11-03 17:52:34 -0600

    case change

--- jstestdriver.server.sh
+++ jstestdriver.server.sh
@@ -1,2 +1,11 @@
 #start the server and launch a browser
-java -jar lib/JsTestDriver-1.2.2.jar --port 4222 --browser open
+#java -jar lib/JsTestDriver-1.2.2.jar --port 4222 --browser open
+
+#start the server
+#java -jar lib/JsTestDriver-1.2.2.jar --port 4222 --browser /Applications/Internet/Firefox.app/Contents/MacOS/firefox-bin -
+java -jar lib/jstestdriver-1.2.2.jar --port 4222
+
+#connect browsers
+#open -a Safari http://localhost:4222/capture
+#open -a "Google Chrome" http://localhost:4222/capture
+#open -a Firefox http://localhost:4222/capture
