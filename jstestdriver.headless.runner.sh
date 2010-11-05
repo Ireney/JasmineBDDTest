2b6f6a359e8b57be83741442fa0900df58a7b29c
Ireney <ireney@navnet.com>
2010-11-04 23:50:09 -0600

    try a different chrome approach

--- jstestdriver.headless.runner.sh
+++ jstestdriver.headless.runner.sh
@@ -28,7 +28,12 @@ PID_XVFB="$!"      # take the process ID
 export DISPLAY=:99 # set display to use that of the xvfb
 
 # run the tests
-java -jar lib/jstestdriver-1.2.2.jar --config jsTestDriver.conf --port 4222 --browser $CHROME --tests all --testOutput $OUTPUT_DIR
+#java -jar lib/jstestdriver-1.2.2.jar --config jsTestDriver.conf --port 4222 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
+
+#start the server
+java -jar lib/jstestdriver-1.2.2.jar --port 4222 --browser $FIREFOX
+$CHROME http://localhost:4222/capture
+java -jar lib/jstestdriver-1.2.2.jar --tests all --testOutput test-results
 
 kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
 echo "Done."
