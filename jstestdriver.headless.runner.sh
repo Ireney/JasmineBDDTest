9f996037d438ec2812fabc10b8f5df9a160e87cf
Ireney <ireney@navnet.com>
2010-11-05 00:52:24 -0600

    revert

--- jstestdriver.headless.runner.sh
+++ jstestdriver.headless.runner.sh
@@ -35,12 +35,12 @@ PID_XVFB="$!"      # take the process ID
 export DISPLAY=:99 # set display to use that of the xvfb
 
 # run the tests
-#java -jar lib/jstestdriver-1.2.2.jar --config jsTestDriver.conf --port 4222 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
+java -jar lib/jstestdriver-1.2.2.jar --config jsTestDriver.conf --port 4222 --browser $FIREFOX --tests all --testOutput $OUTPUT_DIR
 
 #start the server
-$JAVA -jar lib/jstestdriver-1.2.2.jar --port 4222 --browser $FIREFOX
+#$JAVA -jar lib/jstestdriver-1.2.2.jar --port 4222 --browser $FIREFOX
 #$CHROME --no-first-run --homepage localhost:4222/capture
-$JAVA -jar lib/jstestdriver-1.2.2.jar --tests all --testOutput test-results
+#$JAVA -jar lib/jstestdriver-1.2.2.jar --tests all --testOutput test-results
 
 kill $PID_XVFB     # shut down xvfb (firefox will shut down cleanly by JsTestDriver)
 echo "Done."
